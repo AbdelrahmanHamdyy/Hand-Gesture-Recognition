e9b9109583c4deb6d78ef2bc3f18f6e14a58de4a
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-05-01 23:14:40 +0300

    Show images

--- src/utils.py
+++ src/utils.py
@@ -9,3 +9,11 @@ def readImages(dataPath):
         img = cv.imread(dataPath + "/" + fileName, 0)
         x.append(img)
     return x
+
+def showImages(imgs, labels):
+    if len(imgs) != len(labels):
+        print("Length of images isn't the same as labels")
+        return
+    for i in range(len(imgs)):
+        cv.imshow(labels[i], imgs[i])
+        cv.waitKey(0)
