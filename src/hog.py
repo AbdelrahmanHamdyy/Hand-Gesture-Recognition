d4418a8db7889cfc310463f0f7705e97c5afd2e2
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-05-01 22:22:15 +0300

    Re-organize hog code

--- src/hog.py
+++ src/hog.py
@@ -1,33 +1,23 @@
 import cv2 as cv
 import numpy as np
-from os import listdir
-from os.path import isfile, join
+from utils import readImages
 from skimage.transform import resize
 from skimage.feature import hog
 
 INPUT_PATH = "../input/"
 
-def prepareData(dataPath, files):
-    x = []
-    for fileName in files:
-        img = cv.imread(dataPath + "/" + fileName, 0)
-        x.append(img)
-    return x
-
-def train(x_train):
-    list_hog_fd = []
+def getFeatures(x_train):
+    features = []
     for img in x_train:
         fd = hog(resize(img, (128*4, 64*4)), orientations=9, pixels_per_cell=(14, 14),
                  cells_per_block=(1, 1), visualize=False)
-        list_hog_fd.append(fd)
+        features.append(fd)
 
-    x_train = np.array(list_hog_fd)
-    return x_train
+    return np.array(features)
     
 def run():
-    input = [f for f in listdir(INPUT_PATH) if isfile(join(INPUT_PATH, f))]
-    imgs = prepareData(INPUT_PATH, input)
-    result = train(imgs)
+    imgs = readImages(INPUT_PATH)
+    result = getFeatures(imgs)
     print(result)
     
 if __name__ == '__main__':
