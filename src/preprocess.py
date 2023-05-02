624053fc1f747b8c1dbd9d036dfcfa67a2cd0468
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-05-02 23:41:14 +0300

    Best result for preprocessing so far

--- src/preprocess.py
+++ src/preprocess.py
@@ -42,6 +42,36 @@ def preprocess(img):
     
     return normalizedResult
 
+def contours(img):
+    contour,hier = cv.findContours(img,cv.RETR_CCOMP,cv.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contour:
+        cv.drawContours(img, [cnt], 0, 255, -1)
+    
+    return img
+
+def regionFilling(img):
+    # Create a mask with zeros, same size as input image
+    mask = np.zeros((img.shape[0] + 2, img.shape[1] + 2), np.uint8)
+
+    # Set the seed point (x,y) where the fill will start
+    seed_point = (0, 0)
+
+    # Set the fill color (BGR format)
+    fill_color = (0, 255, 0) # Green
+
+    # Specify the lower and upper color range for the fill
+    lower_color = (0, 0, 0)
+    upper_color = (10, 10, 10)
+
+    # Perform the flood fill operation
+    cv.floodFill(img, mask, seed_point, fill_color, lower_color, upper_color, flags=cv.FLOODFILL_MASK_ONLY)
+
+    return img
+
+def restoreImage(mask, img):
+    return cv.bitwise_and(img, img, mask=mask)
+
 def segment(img):
     # Convert image to HSV color space
     hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
@@ -58,17 +88,22 @@ def segment(img):
 
     # Convert img to grayscale
     result = cv.cvtColor(result, cv.COLOR_BGR2GRAY)
-
-    # Morphological Operations
     
-    # Kernel
-    kernel = np.ones((7, 7), np.uint8)
-    kernel2 = np.ones((3, 3), np.uint8)
-    
-    # Apply morphological operations
-    result = cv.dilate(result, kernel, iterations=5)
-    result = cv.erode(result, kernel2, iterations=3)
+    ################### Region Filling using Flood Fill
+    # result = regionFilling(result)
 
+    ################### Morphological Operations
+    kernel = np.ones((11, 11), np.uint8)
+    # kernel2 = np.ones((5, 5), np.uint8)
+    # result = cv.erode(result, kernel2, iterations=5)
+    result = cv.dilate(result, kernel, iterations=7)
+
+    ################### Region Filling using Contours
+    result = contours(result)
+
+    ################### Apply Mask
+    result = restoreImage(result, img)
+    
     # Display result
     # showImages([result], ["Result"])
     
