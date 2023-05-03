8e66a20a10a47bd950d5b834bdb119946daa765f
Zeyad Tarek <zeyad.ta01@gmail.com>
2023-05-03 03:23:04 +0300

    extract features and train the model with hog

--- src/hog.py
+++ src/hog.py
@@ -1,36 +1,65 @@
 import cv2 as cv
 import numpy as np
-from os import listdir
+from os import listdir, scandir
 from os.path import isfile, join
 from skimage.transform import resize
 from skimage.feature import hog
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+import joblib
+# from numba import jit, cuda
+from sklearn import metrics
 
 INPUT_PATH = "../input/"
 
-def prepareData(dataPath, files):
-    x = []
-    for fileName in files:
-        img = cv.imread(dataPath + "/" + fileName, 0)
-        x.append(img)
-    return x
 
-def train(x_train):
-    list_hog_fd = []
-    for img in x_train:
-        fd = hog(resize(img, (128*4, 64*4)), orientations=9, pixels_per_cell=(14, 14),
-                 cells_per_block=(1, 1), visualize=False)
-        list_hog_fd.append(fd)
+def extract_features(data_root_dir):
+    features_list = []
+    labels_list = []
+    for outer_folder in listdir(data_root_dir):
+        # outer folder is considered as the label
+        for inner_file in scandir(join(root_dir, outer_folder)):
+            image = cv.imread(inner_file.path, 0)
+            print(inner_file.path)
+            image_features = hog(resize(image, (128*4, 64*4)), orientations=9, pixels_per_cell=(14, 14),
+                                 cells_per_block=(1, 1), visualize=False)
+            features_list.append(image_features)
+            labels_list.append(outer_folder)
+    return features_list, labels_list
 
-    x_train = np.array(list_hog_fd)
-    return x_train
-    
-def run():
-    input = [f for f in listdir(INPUT_PATH) if isfile(join(INPUT_PATH, f))]
-    imgs = prepareData(INPUT_PATH, input)
-    result = train(imgs)
-    print(result)
-    
+
+def train_model(features_list, labels_list):
+    X_train, X_test, y_train, y_test = train_test_split(
+        features_list, labels_list, test_size=0.33)
+    # train using K-NN
+    knn = KNeighborsClassifier(n_neighbors=3)
+    knn.fit(X_train, y_train)
+    # get the model accuracy
+    model_score = knn.score(X_test, y_test)
+    print(model_score)
+    # save trained model
+    joblib.dump(knn, './models/knn_model_digits.pkl')
+
+
+def train_model_svm(features_list, labels_list):
+    svm_model = SVC(kernel='linear')
+    X_train, X_test, y_train, y_test = train_test_split(
+        features_list, labels_list, test_size=0.33)
+    # fitting x samples and y classes
+    print("y train", y_train)
+    svm_model.fit(X_train, y_train)
+    # save trained model
+    joblib.dump(svm_model, './models/svm_model_digits.pkl')
+    y_pred = svm_model.predict(X_test)
+    print(labels_list)
+    print("==============================================")
+    print(y_pred)
+    print(y_test)
+    print("Accuracy:", metrics.accuracy_score(y_pred, y_test))
+
+
 if __name__ == '__main__':
-    run()
-    
-    
+    root_dir = "input"
+    features_list, labels_list = extract_features(root_dir)
+    train_model(features_list, labels_list)
