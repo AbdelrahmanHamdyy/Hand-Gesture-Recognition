013020263d32b37516bb7dc757569b54f6996060
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-05-06 20:58:13 +0300

    KNN and SVM model functions

--- src/hog.py
+++ src/hog.py
@@ -12,6 +12,7 @@ from sklearn.model_selection import train_test_split
 INPUT_PATH = "../Dataset_0-5/"
 featuresDict = {}
 
+
 def lbp(img, radius=3, n_points=8):
     """
     Compute Local Binary Pattern (LBP) features for an image.
@@ -29,6 +30,7 @@ def lbp(img, radius=3, n_points=8):
     hist = hist.astype("float")
     hist /= hist.sum() + 1e-7
     return hist
+
 
 def hog_features(img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(3, 3)):
     """
@@ -49,55 +51,76 @@ def hog_features(img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(3
     )
     return hog_feats
 
+
 def applyPCA(features):
     pca = PCA(n_components=172)  # keep the top N principal components
     pca.fit(features)
     featuresPCA = pca.transform(features)
     return featuresPCA
 
+
 def getFeatures(x_train):
     features = []
     for img in x_train:
-        img=segment(img)
+        img = segment(img)
 
         # fd = hog_features(resize(img, (128*4, 64*4)))
-        fd = hog(resize(img, (128*4, 64*4)), orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualize=False)
+        fd = hog(resize(img, (128*4, 64*4)), orientations=9,
+                 pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualize=False)
         # fd = lbp(resize(img, (128*4, 64*4)))
         features.append(fd)
-        
-    fd_PCA = applyPCA(features)
-    return fd_PCA
-    
+
+    # fd_PCA = applyPCA(features)
+    return features
+
+
 def saveFeatures():
     x = []
     y = []
-    for i in range (6):
+    for i in range(6):
         print(i)
         print("------------------------------")
         INPUT_PATH = "../Dataset_0-5/"+str(i)
         imgs = readImages(INPUT_PATH)
-        features=getFeatures(imgs)
-        
+        features = getFeatures(imgs)
+
         featuresDict[str(i)] = features
         x = x + features
-        y = y + ([str(i)]  * len(features))
-        
-    return x, y
-        
-def run():
-    x, y = saveFeatures()
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+        y = y + ([str(i)] * len(features))
 
+    return x, y
+
+
+def KNN(x_train, x_test, y_train, y_test):
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(x_train, y_train)
+
+    # Make predictions on the test data and evaluate the model
+    y_pred = knn.predict(x_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print('Accuracy: {:.2f}'.format(accuracy))
+
+
+def SVM(x_train, x_test, y_train, y_test):
     # Train an SVM model using the training data
     svm_model = SVC(kernel='linear', C=1.0)
-    svm_model.fit(X_train, y_train)
+    svm_model.fit(x_train, y_train)
 
     # Predict the labels for the test data
-    # y_pred = svm_model.predict(X_test)
+    # y_pred = svm_model.predict(x_test)
     # accuracy = accuracy_score(y_test, y_pred)
-    accuracy = svm_model.score(X_test, y_test)
+    accuracy = svm_model.score(x_test, y_test)
     print('Accuracy: {:.2f}'.format(accuracy))
+
+
+def run():
+    x, y = saveFeatures()
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.2, random_state=42)
+
+    SVM(x_train, x_test, y_train, y_test)
     # saveToExcel(featuresDict,"../output.xlsx")
-    
+
+
 if __name__ == '__main__':
     run()
