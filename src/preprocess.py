5c1bbe90e1b03d4b58913f6bcf68e5bd42bfbe54
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-05-06 20:59:31 +0300

    75% Accuracy for men and women
    Approach: Bigger dilation kernel

--- src/preprocess.py
+++ src/preprocess.py
@@ -7,6 +7,7 @@ from utils import *
 
 mpl.rcParams['image.cmap'] = 'gray'
 
+
 def preprocess(img):
 
     # Convert img to grayscale
@@ -14,41 +15,45 @@ def preprocess(img):
 
     # Convert to black & white
     # (thresh, img) = cv.threshold(img, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
-    
+
     # Smooth the image using a gaussian filter
     img = cv.GaussianBlur(img, (5, 5), 0)
-    
+
     # Smooth the image using a median filter
     # img = cv.medianBlur(img, 5)
-    
+
     # Remove shadows
     rgbPlanes = cv.split(img)
 
     planes = []
     normalizedPlanes = []
     for plane in rgbPlanes:
-        dilatedImg = cv.dilate(plane, np.ones((7,7), np.uint8))
+        dilatedImg = cv.dilate(plane, np.ones((7, 7), np.uint8))
         blurImg = cv.medianBlur(dilatedImg, 21)
         diffImg = 255 - cv.absdiff(plane, blurImg)
-        normalizedImg = cv.normalize(diffImg, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_8UC1)
+        normalizedImg = cv.normalize(
+            diffImg, None, alpha=0, beta=255, norm_type=cv.NORM_MINMAX, dtype=cv.CV_8UC1)
         planes.append(diffImg)
         normalizedPlanes.append(normalizedImg)
-    
+
     result = cv.merge(planes)
     normalizedResult = cv.merge(normalizedPlanes)
-    
+
     # Show images
-    showImages([img, result, normalizedResult], ["Input", "Result", "Norm Result"])
-    
+    showImages([img, result, normalizedResult], [
+               "Input", "Result", "Norm Result"])
+
     return normalizedResult
 
+
 def contours(img):
-    contour,hier = cv.findContours(img,cv.RETR_CCOMP,cv.CHAIN_APPROX_SIMPLE)
+    contour, hier = cv.findContours(img, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
 
     for cnt in contour:
         cv.drawContours(img, [cnt], 0, 255, -1)
-    
+
     return img
+
 
 def regionFilling(img):
     # Create a mask with zeros, same size as input image
@@ -58,19 +63,22 @@ def regionFilling(img):
     seed_point = (0, 0)
 
     # Set the fill color (BGR format)
-    fill_color = (0, 255, 0) # Green
+    fill_color = (0, 255, 0)  # Green
 
     # Specify the lower and upper color range for the fill
     lower_color = (0, 0, 0)
     upper_color = (10, 10, 10)
 
     # Perform the flood fill operation
-    cv.floodFill(img, mask, seed_point, fill_color, lower_color, upper_color, flags=cv.FLOODFILL_MASK_ONLY)
+    cv.floodFill(img, mask, seed_point, fill_color, lower_color,
+                 upper_color, flags=cv.FLOODFILL_MASK_ONLY)
 
     return img
 
+
 def restoreImage(mask, img):
     return cv.bitwise_and(img, img, mask=mask)
+
 
 def segment(img):
     # Convert image to HSV color space
@@ -89,44 +97,46 @@ def segment(img):
     # Convert img to grayscale
     result = cv.cvtColor(result, cv.COLOR_BGR2GRAY)
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    
-    ################### Histogram Equalization
+
+    # Histogram Equalization
     # img = cv.equalizeHist(img)
-    
+
     # ################### Gaussian filter
     # img = cv.GaussianBlur(img, (9, 9), 0)
-    
-    ################### Region Filling using Flood Fill
+
+    # Region Filling using Flood Fill
     # result = regionFilling(result)
 
-    ################### Morphological Operations
-    kernel = np.ones((11, 11), np.uint8)
+    # Morphological Operations
+    kernel = np.ones((24, 24), np.uint8)
     # kernel2 = np.ones((16, 16), np.uint8)
-    result = cv.dilate(result, kernel, iterations=7)
-    
-    ################### Region Filling using Contours
-    result = contours(result)
-    # result = cv.erode(result, kernel2, iterations=12)
+    result = cv.dilate(result, kernel, iterations=20)
 
-    ################### Apply Mask
+    # Region Filling using Contours
+    result = contours(result)
+    # result = cv.erode(result, kernel2, iterations=7)
+
+    # Apply Mask
     result = restoreImage(result, img)
-    
+
     # Display result
     # showImages([result], ["Result"])
-    
+
     return result
+
 
 def graySegment(img):
     # Convert img to grayscale
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    
+
     cv.imwrite("../output/gray.jpg", img)
-    
+
     # Filter
     gray_filtered = cv.inRange(img, 190, 255)
-    
+
     cv.imwrite("../output/filtered_gray.jpg", gray_filtered)
-    
+
+
 def gaussianMixture(img):
     # Convert to the YCrCb color space
     img_ycrcb = cv.cvtColor(img, cv.COLOR_BGR2YCrCb)
@@ -153,30 +163,34 @@ def gaussianMixture(img):
     result = cv.bitwise_and(img, img, mask=np.uint8(mask * 255))
     return result
 
+
 def runSegmentation():
     # img = cv.imread("../input/3.jpeg")
     imgs = readImages("../input/")
     # preprocess(img)
     # result = gaussianMixture(img)
     for i in range(len(imgs)):
-        segmentationResult = segment(imgs[i]) 
+        segmentationResult = segment(imgs[i])
         cv.imwrite("../output2/result" + str(i) + ".jpeg", segmentationResult)
     # showImages([segmentationResult], ["Result"])
+
 
 def adaptiveThresholding(img):
     # Convert img to grayscale
     img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    adaptive_thresh = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+    adaptive_thresh = cv.adaptiveThreshold(
+        img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
     return adaptive_thresh
+
 
 def test():
     img = cv.imread("../input/1_men (2).JPG")
-    result = segment(img) 
+    result = segment(img)
     # result = adaptiveThresholding(img)
     cv.imwrite("../output/result.jpeg", result)
     # graySegment(img)
 
+
 if __name__ == '__main__':
     runSegmentation()
     # test()
-    
